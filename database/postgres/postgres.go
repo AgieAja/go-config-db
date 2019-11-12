76728d8fe45c49f983b660518ea6d48fa3e7f59d
agie <tiar.agisti@gmail.com>
2019-11-12 21:42:30 +0700

    adding function connect ORM  postgresSQL

---
+++ database/postgres/postgres.go
@@ -0,0 +1,47 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	//dont delete this package..driver for connection postgreSQL
+	_ "github.com/lib/pq"
+)
+
+var (
+	sqlDbORM  *gorm.DB
+	sqlORMErr error
+)
+
+//createConnPostgresORM - create connection database postgresSQL
+func createConnPostgresORM(desc string) (*gorm.DB, error) {
+	// val := url.Values{}
+	// val.Add("loc", "Asia/Jakarta")
+	// dsn := fmt.Sprintf("%s&%s", desc, val.Encode())
+	sqlDbORM, err := gorm.Open(`postgres`, desc)
+	if err != nil {
+		return nil, err
+	}
+
+	err = sqlDbORM.DB().Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDbORM.DB().SetMaxIdleConns(10)
+	sqlDbORM.DB().SetMaxOpenConns(10)
+
+	return sqlDbORM, nil
+}
+
+//InitConnPostgresSQLDBORM - preparetion connection database postgresSQL
+func InitConnPostgresSQLDBORM(dbHost, dbPort, dbUser, dbPass, dbName string) {
+	desc := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbName)
+
+	sqlDbORM, sqlORMErr = createConnPostgresORM(desc)
+}
+
+//GetPostgresSQLDBORM - get connection db postgres
+func GetPostgresSQLDBORM() (*gorm.DB, error) {
+	return sqlDbORM, sqlORMErr
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/golangci/golangci-lint v1.21.0 // indirect
 	github.com/jinzhu/gorm v1.9.11
+	github.com/lib/pq v1.2.0
+	golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392 // indirect
 )
